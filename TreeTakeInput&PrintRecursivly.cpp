b9b952823e029c9eeb26c1a63267cb5fd3f3c2d2
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-09-30 11:44:43 +0530

    Update TreeTakeInput&PrintRecursivly.cpp

--- TreeTakeInput&PrintRecursivly.cpp
+++ TreeTakeInput&PrintRecursivly.cpp
@@ -1,2 +1,15 @@
 
 // Basic Tree example of taking input recursivly & printing output recursivly in c++ programming using vector
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+template<typename T> class Tree{
+  public:
+  T data;
+  vector<Tree<T>*>childrean;
+  
+  Tree(T data){
+    this->data=data;
+  }
+};
