aedd1fdfd1f286d5381ca55fc2c3100b3e45d94a
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-09-30 11:59:15 +0530

    Update TreeTakeInput&PrintRecursivly.cpp

--- TreeTakeInput&PrintRecursivly.cpp
+++ TreeTakeInput&PrintRecursivly.cpp
@@ -4,12 +4,46 @@
 #include<vector>
 using namespace std;
 
-template<typename T> class Tree{
+template<typename T> class TreeNode{
   public:
   T data;
-  vector<Tree<T>*>childrean;
+  vector<TreeNode<T>*>childrean;
   
   Tree(T data){
     this->data=data;
   }
 };
+TreeNode<int>* takeInput(){
+  int rootData;
+  cout<<"Enter data: "<<endl;
+  cin>>rootData;
+  TreeNode<int>* root=new TreeNode(rootData);
+  int n;
+  cout<<"Enter number of childrean: "<<endl;
+  for(int i=0;i<n;i++){
+    TreeNode<int>* child=takeInput();
+    root->childrean.push_back(child);
+  }
+return root;
+}
+void printTree(TreeNode<int>* root){
+  if(root==NULL){
+    return;
+  }
+  cout<<root->data<<": ";
+  for(int i=0;i<root->childrean.size();i++){
+    cout<<root->childrean[i]->data<<", ";
+  }
+  cout<<endl;
+  for(int i=0;i<root->childrean.size();i++){
+    printTree(root->childrean[i]);
+  }
+  
+}
+
+int main(){
+
+  TreeNode<int>* root=takeInput();
+  printTree(root);
+  return 0;
+}
